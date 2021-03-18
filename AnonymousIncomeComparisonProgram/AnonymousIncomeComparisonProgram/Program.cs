af15a0e181b24b0d906a2824488ed9388a86fd9d
Libbybacon <libbybacon@github.com>
2021-03-18 16:39:28 -0400

    Anonymous Income Comparison updated
    Made code more succinct and fixed comparison from person2 > person1 to person1 > person2

--- AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
+++ AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
@@ -9,25 +9,21 @@ namespace AnonymousIncomeComparisonProgram
         {
             Console.WriteLine("Anonymous Income Comparison Program\nPerson 1:");
             Console.WriteLine("What is your hourly rate?");
-            string hourlyWageRate = Console.ReadLine();
-            int hourlyRate = Convert.ToInt32(hourlyWageRate);
+            int hourlyRate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours do you work per week?");
-            string weeklyHours = Console.ReadLine();
-            int hoursPerWeek = Convert.ToInt32(weeklyHours);
+            int hoursPerWeek = Convert.ToInt32(Console.ReadLine());
             int weekSalary = hourlyRate * hoursPerWeek;
             int annualSalary = weekSalary * 52;
 
             Console.WriteLine("Person 2:");
             Console.WriteLine("What is your hourly rate?");
-            string hourlyWageRate2 = Console.ReadLine();
-            int hourlyRate2 = Convert.ToInt32(hourlyWageRate2);
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours do you work per week?");
-            string weeklyHours2 = Console.ReadLine();
-            int hoursPerWeek2 = Convert.ToInt32(weeklyHours2);
+            int hoursPerWeek2 = Convert.ToInt32(Console.ReadLine());
             int weekSalary2 = hourlyRate2 * hoursPerWeek2;
             int annualSalary2 = weekSalary2 * 52;
 
-            bool salaryComparison = annualSalary2 > annualSalary;
+            bool salaryComparison = annualSalary > annualSalary2;
 
             Console.WriteLine("Annual salary of person 1: " + annualSalary);
             Console.WriteLine("Annual salary of person 2: " + annualSalary2);
