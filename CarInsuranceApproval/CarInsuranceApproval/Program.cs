b40426e1c9dacd93ff6bdd176b7cc1935eb2d63e
Libbybacon <libbybacon@github.com>
2021-03-18 17:06:46 -0400

    Car Insurance App Practice
    Prompt user to enter details about age, DUIs and Speeding tickets to determine whether they are qualified for insurance.

---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Prompt user with questions about driving status to determine whether they are qualified for insurance
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? please answer true or false.");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedTix = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = age > 15 && DUI == false && speedTix <= 3;
+            Console.WriteLine("This driver is qualified: " + qualified);
+            Console.ReadLine();
+
+
+        }
+    }
+}
