05513428e83b94f673299a250e59d11f4c141bef
Libbybacon <libbybacon@github.com>
2021-03-23 23:14:07 -0400

    BlackJack game
    Intro to objects and classes

---
+++ 21GameClassesAndObjects/21GameClassesAndObjects/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21GameClassesAndObjects
+{
+    public class Card
+    {
+        //public Card() 
+        //{
+        //    Suit = "Spades";
+        //    Face = "Two";
+        //}
+        public string Face { get; set; }
+        public string Suit { get; set; }
+    }
+}
---
+++ 21GameClassesAndObjects/21GameClassesAndObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21GameClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+                Console.ReadLine();
+            }
+
+           Console.WriteLine(deck.Cards.Count);
+           Console.ReadLine();
+
+           
+        }
+    }
+}
