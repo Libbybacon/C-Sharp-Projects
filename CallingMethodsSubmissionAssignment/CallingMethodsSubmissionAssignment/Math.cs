a2e2c3a8f2b80f460663deb85dd930acf056895c
Libbybacon <libbybacon@github.com>
2021-03-24 13:48:35 -0400

    CallingMethodsSubmissionAssignment
    Create a new class that contains three basic methods for performing math operations.  Call methods in main program and take user input as parameter for methods.  Display results

---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    public class Math
+    {
+        //Create three methods, each one takes one integer parameter in and returns an integer
+        public static int AddTwo(int num)
+        {
+            return num + 2;
+
+        }
+
+        public static int DivideByTwo(int num)
+        {
+            return num / 2;
+        }
+
+        public static int MultiplyByTwo(int num)
+        {
+            return num * 2;
+        }
+    }
+}
---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask user for a number to perform math operations on
+            Console.WriteLine("Please enter a number to do math operations on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Call each method in turn, passing user input to the method.  Display returned int
+            int sum = Math.AddTwo(userNum);
+            Console.WriteLine(userNum + " plus two equals: " + sum);
+
+            int product = Math.MultiplyByTwo(userNum);
+            Console.WriteLine(userNum + " times two equals: " + product);
+
+            int quotient = Math.DivideByTwo(userNum);
+            Console.WriteLine(userNum + " divided by two equals: " + quotient);
+
+            Console.ReadLine();
+        }
+    }
+}
