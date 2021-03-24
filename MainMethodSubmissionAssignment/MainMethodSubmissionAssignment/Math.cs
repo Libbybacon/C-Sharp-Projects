117e5ad15e0fdee5af1bdd2418e291dba1cd3707
Libbybacon <libbybacon@github.com>
2021-03-24 15:39:20 -0400

    Main Method Submission Assignment
    Create a new class, within that class create three methods with the same names that each take in different argument types and/or perform different math functions, all return an integer vale.  Call each method in the main program.

---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Math
+    {
+        //Create a method that will take in an integer and perform a math operation
+        public static int Operation(int num1, int num2)
+        {
+            int product = num1 * num2;
+            return product;
+        }
+
+        //Create second method with the same name that will take in a decimal,
+        //create a different math operation for it, return answer as integer
+        public static decimal Operation(decimal num)
+        {
+            int square = Convert.ToInt32(num * num);
+            return square;
+        }
+
+        //Create a third method with the same name that will take in a string,
+        //convert it to an integer if possible, do a different math operation on it,
+        //return answer as int
+        public static int Operation(string num)
+        {
+            int half = Convert.ToInt32(num) / 2;
+            return half;
+        }
+    }
+}
---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call Math method 'Operation', pass in user's numbers as arguments and display result
+            Console.WriteLine("Please enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            int product = Math.Operation(num1, num2);
+            
+            Console.WriteLine(num1 + " times " + num2 + " equals: " + product);
+            
+
+            //Call second Math method 'Operation', pass in user's decimal value as argument, return int.
+            Console.WriteLine("Please enter a decimal value: ");
+            decimal num3 = Convert.ToDecimal(Console.ReadLine());
+
+            int square = (int)Math.Operation(num3);
+
+            Console.WriteLine(num3 + " squared equals: " + square);
+
+            //Call third Math method 'Operation', pass in user's string value as argument, return int.
+            Console.WriteLine("Please enter a number: ");
+            string num = Console.ReadLine();
+
+            int half = Convert.ToInt32(num) / 2;
+
+            Console.WriteLine("Half of " + num + " is: " + half);
+            Console.ReadLine();
+        }
+    }
+}
