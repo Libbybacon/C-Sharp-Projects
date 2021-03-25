6e38b1b0869aea9cf3dd257369a3993cdf3b8942
Libbybacon <libbybacon@github.com>
2021-03-25 14:49:06 -0400

    Optional Parameter Submission Assignment
    Create a new class, create a method in that class with an optional parameter.
    Instantiate class in main program
    as user for two inputs (second optional)
    call method and print results depending on user input

---
+++ OptionalParameterMethodSubmissionAssignment/OptionalParameterMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodSubmissionAssignment
+{
+    public class Math
+    {
+        //Create a method that takes two ints as parameters
+        //Make one of them optional
+        //Have the method do a math operation and return in result
+        public int Multiply(int num1, int num2 = 2)
+        {
+            int product = num1 * num2;
+            return product;
+
+        }
+
+    }
+}
---
+++ OptionalParameterMethodSubmissionAssignment/OptionalParameterMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate class "Math"
+            Math myMath = new Math();
+            int num1;
+            int num2;
+            int product;
+
+            //Ask user to input a number:
+            Console.WriteLine("Please enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Ask user for second, optional, number:
+            Console.WriteLine("Please enter a second number (optional): ");
+            
+            //Check if user entered number:
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                product = myMath.Multiply(num1, num2);
+                Console.WriteLine(num1 + " times " + num2 + " equals: " + product);
+            }
+
+            else
+            {
+                product = myMath.Multiply(num1);
+                Console.WriteLine(num1 + " times two equals: " + product);
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
