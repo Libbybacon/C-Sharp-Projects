5c953e1b6cdd912b733d6a48bcbebc2f3d503646
Libbybacon <libbybacon@github.com>
2021-03-25 15:04:33 -0400

    Void Method Submission Assignment
    Create a class
    In that class create a void method that takes in to int parameters
    do math operation on first int, display second to screen
    In main program, instantiate class and call method with 2 numbers and then with variable names

---
+++ VoidMethodSubmissionAssignment/VoidMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math myMath = new Math();
+            int num1 = 2;
+            int num2 = 6;
+
+            myMath.Square(5, 3);
+            myMath.Square(num1, num2);
+
+        }
+    }
+}
