aac6f1257d7d0c72242a9d4cf879d80fb62251c1
Libbybacon <libbybacon@github.com>
2021-03-25 15:06:23 -0400

    Void Method Submission Assignment
    Added comments

--- VoidMethodSubmissionAssignment/VoidMethodSubmissionAssignment/Program.cs
+++ VoidMethodSubmissionAssignment/VoidMethodSubmissionAssignment/Program.cs
@@ -10,11 +10,15 @@ namespace VoidMethodSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //Instantiate class
             Math myMath = new Math();
             int num1 = 2;
             int num2 = 6;
 
+            //Call method passing in two numbers
             myMath.Square(5, 3);
+
+            //Call method specifying parameters by name
             myMath.Square(num1, num2);
 
         }
