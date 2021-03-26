a83948c49255d9fcc919dfc6deba0ae44be23837
Libbybacon <libbybacon@github.com>
2021-03-26 19:24:19 -0400

    AbstractClassSubmissionAssignment generic types
    Instantiate two different objects of a class, each with different generic type

--- AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
+++ AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 namespace AbstractClassSubmissionAssignment
 {
     //Employee class inherits Person class and IQuittable interface
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
+        //Give Employee ID property
+        public int ID { get; set; }
+
+        //Add property 'things' and have data type be generic list matching generic type of class
+        public List<T> Things {get; set;}
         
 
         //Implement SayName() method 
@@ -24,5 +29,33 @@ namespace AbstractClassSubmissionAssignment
         {
             Console.WriteLine("This place sucks, I quit!");
         }
+
+        ////Overload '==' operator so it checks if two Employee objects are equal by comparing ID property
+        //public static bool operator== (Employee employee1, Employee employee2)
+        //{
+        //     if (employee1.ID == employee2.ID)
+        //    {
+        //        return true;
+        //    }
+        //     else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //////Overload '!=' operator because you have to if you're going to change the '==' operator.  I think...
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.ID != employee2.ID)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
     }
 }
--- AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Program.cs
+++ AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Program.cs
@@ -10,12 +10,38 @@ namespace AbstractClassSubmissionAssignment
     {
         static void Main(string[] args)
         {
-           
-            //Use polymorphism to create object of type IQuittable, 
-            IQuittable employee = new Employee() { firstName = "Sample", lastName = "Student"};
+
+            //Use polymorphism to create object of type IQuittable
+            IQuittable employee = new Employee<string>() { firstName = "Sample", lastName = "Student" };
+
             //Call Quit() method on IQuittable object
             employee.Quit();
 
+
+
+            //Instantiate Employee object with type "string" as generic parameter
+            Employee<string> employee1 = new Employee<string>();
+
+            //Assign a list of strings as the property value of Things
+            employee1.Things = new List<string>() {"Office", "Computer", "Desk", "Chair", "Phone" };
+
+            //Instantiate Employee object with type "int" as generic parameter
+            Employee<int> employee2 = new Employee<int>();
+
+            //Assign list of integers as the property value of Things
+            employee2.Things = new List<int>() { 13, 7, 14, 21, 3};
+
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            
             Console.ReadLine();
 
 
