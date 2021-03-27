46e30dfc2f6ce1ed4b57c52f55672a29a932da46
Libbybacon <libbybacon@github.com>
2021-03-27 16:31:33 -0400

    Struct Practice Assignment
    Create a struct
    give it a property
    create object of struct data type and assign value to it

---
+++ StructPracticeAssignment/StructPracticeAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPracticeAssignment
+{
+    class Program
+    {
+        //Create struct called Number
+        public struct Number
+        {
+            //Give it property 'Amount' of type decimal
+            public decimal Amount;
+        }
+
+        static void Main(string[] args)
+        {
+            //Create object of data type Number and assign amount to it
+            Number number1;
+            number1.Amount = 3;
+            Console.WriteLine("The amount of number1 = " + number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
