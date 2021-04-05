bdfa8e50bbf1f3aba6fba4adb60480f1025826ce
Libbybacon <libbybacon@github.com>
2021-04-05 14:21:07 -0400

    practicing code first
    create very basic code first console app utilizing entity framework.  Make a new database and add one item to it

---
+++ PracticeCodeFirstApp2/PracticeCodeFirstApp2/Grade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCodeFirstApp2
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ PracticeCodeFirstApp2/PracticeCodeFirstApp2/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PracticeCodeFirstApp2
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
